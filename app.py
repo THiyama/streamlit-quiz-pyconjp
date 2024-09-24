78f7349e61008cb7f91ec334ed22c95b81ad56f9
THiyama <robot_toru@outlook.jp>
2024-09-24 23:46:08 +0900

    ID重複時の挙動を修正＆リファクタ

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import streamlit as st
+import snowflake.snowpark.functions as F
 from snowflake.snowpark import Session
 
 from utils.utils import (
     create_session,
     display_team_id_sidebar,
     display_page_titles_sidebar,
-    prompt_for_duplicate_username_continue,
-    is_team_id_exists,
     TEAMS,
 )
 from utils.designs import (
@@ -16,11 +15,38 @@ from utils.designs import (
 )
 
 
-display_page_titles_sidebar()
+@st.dialog("ユーザー名の確認")
+def show_duplicate_username_dialog(team_id: str) -> None:
+    st.write("ユーザー名がすでに登録されているようです。")
+    st.write("このまま続けますか？")
+    st.session_state.is_ignoring_duplicates = False
+
+    col1, col2 = st.columns(2)
+    if col1.button("はい"):
+        st.session_state.is_ignoring_duplicates = True
+        st.rerun()
+
+    if col2.button("いいえ"):
+        st.session_state.is_ignoring_duplicates = False
+        st.rerun()
+
+
+def confirm_duplicate_username() -> None:
+    st.session_state.is_submitted_main_page = True
+
+    session = st.session_state.snow_session
+    team_id = st.session_state.team_id
+
+    is_duplicated = session.table("Submit2").filter(F.col("TEAM_ID") == team_id).count()
+
+    if is_duplicated:
+        show_duplicate_username_dialog(team_id)
+    else:
+        if "is_ignoring_duplicates" in st.session_state:
+            del st.session_state.is_ignoring_duplicates
 
 
 st.title("Streamlit クイズへようこそ！")
-display_team_id_sidebar()
 
 css_name = apply_default_custom_css()
 message = f"""
@@ -35,39 +61,34 @@ message = f"""
     **さあ、クイズに挑戦しましょう！**
     """
 
+display_page_titles_sidebar()
+display_team_id_sidebar()
 display_applied_message(message, css_name)
 background_image("pages/common/images/background1.jpg", dark_mode=False)
 
 st.write("")
 
-
-snow_session = create_session(TEAMS["Account Admin"])
-st.session_state.snow_session = snow_session
-
 team_id = st.text_input(label="ユーザー名を入力してください。")
 
-if is_team_id_exists(snow_session, team_id):
-    if f"is_prompt_for_{team_id}" not in st.session_state:
-        prompt_for_duplicate_username_continue(team_id)
-
-    else:
-        if not st.session_state[f"is_prompt_for_{team_id}"]:
-            st.warning("ユーザー名を変更してください。")
-            st.stop()
-
-else:
-    keys_to_clear = [
-        key for key in st.session_state if key.startswith("is_prompt_for_")
-    ]
-
-    for key in keys_to_clear:
-        del st.session_state[key]
-
+st.session_state.snow_session = snow_session = create_session(TEAMS["Account Admin"])
 st.session_state.team_id = team_id
 
-if st.button("挑戦を開始する"):
-    print(st.session_state)
+if "is_submitted_main_page" not in st.session_state:
+    st.session_state.is_submitted_main_page = False
+
+st.button("挑戦を開始する", on_click=confirm_duplicate_username)
+
+print()
+print(
+    f"st.session_state.is_submitted_main_page: {st.session_state.is_submitted_main_page}"
+)
+if st.session_state.is_submitted_main_page:
     if team_id == "":
         st.error("ユーザー名を入力してみましょう！")
     else:
-        st.switch_page("pages/01_normal_problems.py")
+        if "is_ignoring_duplicates" not in st.session_state:
+            st.session_state.is_submitted_main_page = False
+            st.switch_page("pages/01_normal_problems.py")
+        elif st.session_state.is_ignoring_duplicates:
+            st.session_state.is_submitted_main_page = False
+            st.switch_page("pages/01_normal_problems.py")
